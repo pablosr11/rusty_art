633b6c836d054896d36796f84e6fc58f3c3168eb
ps <pablosr11@gmail.com>
2021-10-12 01:11:22 +0100

    parse local json file in json

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use serde::Deserialize;
+
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::Path;
+
+#[derive(Deserialize, Debug)]
+struct Entry {
+    id: u32,
+    token_add: String,
+    number: u32,
+    currency: String,
+    price: f32,
+    link_img: String,
+    for_sale: u8,
+    programId: String,
+    name: String,
+    description: String,
+    escrowAdd: String,
+    seller_address: String,
+    attributes: String,
+    skin: Option<String>,
+    r#type: String,
+    ranking: Option<u16>,
+    buyer_add: Option<String>,
+    blockhash: Option<String>,
+    last_sold_price: Option<f32>,
+}
+
+fn main() {
+    parse("/Users/ps/repos/rusty_art/full.json").unwrap();
+}
+
+fn parse<P: AsRef<Path>>(path: P) -> Result<Vec<Entry>, Box<dyn Error>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let u: Vec<Entry> = serde_json::from_reader(reader)?;
+    Ok(u)
+}
